fb0101a36c89a3af2a7a3d57e289e249d787d431
ju-ki <juki.programming@gmail.com>
2022-10-09 22:36:12 +0900

    標準出力全部と全探索一部をやった

---
+++ jukiya/algo_method_question/212/main.py
@@ -0,0 +1,26 @@
+"""
+https://algo-method.com/tasks/212
+
+### 問題
+$N$ 個の整数 $A_0, A_1, \dots, A_{N-1}$ が与えられます。
+$N$ 個の整数のうち、正の整数が何個あるかを数えるプログラムを作成してください。
+### 入力
+入力は次の形式で与えられます。
+```IOExample
+$N$
+$A_0 A_1 \dots A_{N-1}$
+```
+また、入力される値は次の制約を満たします。
+- $N$ は $1$ 以上 $100$ 以下の整数
+- $A_i$ は $-100$ 以上 $100$ 以下の整数 $(0 \leq i \leq N-1)$
+### 出力
+$N$ 個の整数のうち $0$ より大きいものの個数を出力してください。
+"""
+
+N = int(input())
+A = list(map(int, input().split()))
+counter = 0
+for num in A:
+    if num > 0:
+        counter += 1
+print(counter)
