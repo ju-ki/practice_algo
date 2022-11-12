cd468b0d0fa597e6f4d470a9543dbe75cc820cdf
ju-ki <juki.programming@gmail.com>
2022-11-12 21:17:22 +0900

    正規表現途中まで

---
+++ jukiya/algo_method_question/299/main.py
@@ -0,0 +1,26 @@
+"""
+https://algo-method.com/tasks/299
+
+### 問題文
+文字列 $S$ が与えられます。
+文字列 $S$ が以下の形式を満たすか判定するプログラムを作成してください。
+- $\underbrace{a \cdots a}_{1 \text{個以上} 5 \text{個以下} } \underbrace{b \cdots b}_{10 \text{個} } \underbrace{c \cdots c}_{0 \text{個以上} }$
+### 入力
+入力は以下の形式から与えられます。
+```IOExample
+$S$
+```
+また、入力される値は次の制約を満たします。
+- $S$ は`a`, `b`, `c`からなる長さ $1$ 以上 $100$ 以下の文字列
+### 出力
+$S$ が与えられた形式を満たすならば `Yes` を、満たさないならば `No` を出力してください。
+"""
+
+import re
+
+S = input()
+pattern = r'^a{1,5}b{10}c*$'
+
+ans = re.search(pattern, S)
+
+print("Yes") if ans else print("No")
